1f90f47721b6580a796c2bd385d3fc481a3977fe
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-19 10:48:26 +0000

    * Multisite Module (for 2.3.x) - Control Panel finished * Payload (Preloader & Postloader to be implemented). * Installation tested on 2.3.3

---
+++ trunk/modules/multisite/xoops_version.php
@@ -0,0 +1,8 @@
+<?php
+if( ! defined( 'XOOPS_PATH' ) ) die( 'set XOOPS_PATH into mainfile.php' ) ;
+
+$mydirname = basename( dirname( __FILE__ ) ) ;
+$mydirpath = dirname( __FILE__ ) ;
+
+require XOOPS_PATH.'/modules/multisite/xoops_version.php' ;
+?>
