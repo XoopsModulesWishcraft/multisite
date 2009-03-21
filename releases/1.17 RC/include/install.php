9d48a27a939c38f0434441e0fd4b5b39e1c4a642
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-21 09:29:50 +0000

    1.17 RC - Multisite Module for XOOPS 2.3.x

---
+++ releases/1.17 RC/include/install.php
@@ -0,0 +1,48 @@
+<?php
+// $Id: update.php 2 2005-11-02 18:23:29Z skalpa $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+// Author: Kazumi Ono (AKA onokazu)                                          //
+// URL: http://www.myweb.ne.jp/, http://www.xoops.org/, http://jp.xoops.org/ //
+// Project: The XOOPS Project                                                //
+// ------------------------------------------------------------------------- //
+
+function xoops_module_pre_install_multisite(&$module) {
+
+	global $xoopsDB;
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('config')." ADD COLUMN (`conf_did` INT(10) NOT NULL DEFAULT '0')");
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('newblocks')." ADD COLUMN(domains MEDIUMTEXT)");
+	$result = $xoopsDB->queryF("UPDATE TABLE ".$xoopsDB->prefix('newblocks')." SET domains='|all'");
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('modules')." ADD COLUMN (`hasrss` TINYINT(1) NOT NULL DEFAULT '0')");
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('modules')." ADD COLUMN (`hasatom` TINYINT(1) NOT NULL DEFAULT '0')");		
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('modules')." ADD COLUMN (`hassitemap` TINYINT(1) NOT NULL DEFAULT '0')");
+	$result = $xoopsDB->queryF("ALTER TABLE ".$xoopsDB->prefix('modules')." ADD COLUMN(domains MEDIUMTEXT)");		
+	$result = $xoopsDB->queryF("UPDATE TABLE ".$xoopsDB->prefix('modules')." SET domains='|all'");
+
+	return true;
+		
+}
+
+?>
