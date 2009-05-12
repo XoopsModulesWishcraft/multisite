b90baef08764b7cc4fa392cba8db60662c6204c7
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-05-12 05:57:14 +0000

    xoops_version.php - changes

--- XoopsModules/multisite/trunk/modules/multisite/admin/definesadmin/xoops_version.php
+++ XoopsModules/multisite/trunk/modules/multisite/admin/definesadmin/xoops_version.php
@@ -29,7 +29,7 @@
 // Project: The XOOPS Project                                                //
 // ------------------------------------------------------------------------- //
 
-$modversion['name'] = _MD_AM_PREF;
+$modversion['name'] = _MI_MULTISITE_ADMENU6;
 $modversion['version'] = "";
 $modversion['description'] = "XOOPS Site Preferences";
 $modversion['author'] = "";
@@ -37,9 +37,9 @@ $modversion['credits'] = "The XOOPS Project";
 $modversion['help'] = "preferences.html";
 $modversion['license'] = "GPL see LICENSE";
 $modversion['official'] = 1;
-$modversion['image'] = "pref.gif";
+$modversion['image'] = "defines.gif";
 
 $modversion['hasAdmin'] = 1;
-$modversion['adminpath'] = "admin.php?fct=preferences";
+$modversion['adminpath'] = "admin.php?fct=definesadmin";
 $modversion['category'] = XOOPS_MULTISITE_PREF;
 ?>
