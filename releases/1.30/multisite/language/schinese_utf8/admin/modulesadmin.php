0d2e59d117846c76fffc6dd595d93d39b98e854c
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-12-09 04:34:56 +0000

    Modules Final Releases - Compounds 2.00, Lawsuit 1.12, Multisite 1.30, Spiders 1.10, VidShop 1.60, VJ Mixes 1.20, Wurlf 1.26, xpNews 1.15, xReaggregator 2.25

---
+++ releases/1.30/multisite/language/schinese_utf8/admin/modulesadmin.php
@@ -0,0 +1,300 @@
+<?php
+ // $号： modulesadmin.php 2411年08年11月14号21时01分○七秒ž julionc $ 
+ // _LANGCODE ：英
+ // _CHARSET ： UTF - 8的
+ //译者： XOOPS翻译团队
+
+ // %%%%%%文件名modulesadmin.php %%%%% 
+define("_MD_AM_MODADMIN" ,"模块管理" ) ;
+ 
+define("_MD_AM_MODULE" ,"模块" ) ;
+ 
+define("_MD_AM_NEWDOMAINS" ,"新领域" ) ;
+ 
+define("_MD_AM_VERSION" ,"版本" ) ;
+ 
+define("_MD_AM_LASTUP" ,"最后更新日期" ) ;
+ 
+define("_MD_AM_DEACTIVATED" ,"已停用" ) ;
+ 
+define("_MD_AM_ACTION" ,"行动" ) ;
+ 
+define("_MD_AM_DEACTIVATE" ,"停用" ) ;
+ 
+define("_MD_AM_ACTIVATE" ,"激活" ) ;
+ 
+define("_MD_AM_UPDATE" ,"更新" ) ;
+ 
+define("_MD_AM_DUPEN" ,"重复项目模块表！" ) ;
+ 
+define("_MD_AM_DEACTED" ,"所选择的模块已停用。您现在可以安全地卸载模块。" ) ;
+ 
+define("_MD_AM_ACTED" ,"所选择的模块已启动！" ) ;
+ 
+define("_MD_AM_UPDTED" ,"所选择的模块已更新！" ) ;
+ 
+define("_MD_AM_SYSNO" ,"系统模块不能停用。" ) ;
+ 
+define("_MD_AM_STRTNO" ,"这个模块是您的默认启动页面。请变更启动模块的任何适合您的偏好。" ) ;
+ 
+define("_MD_AM_DOMAINS_IN" ,"域" ) ;
+ 
+ // 添加的RC2测试版
+define("_MD_AM_PCMFM" ,"请确认：" ) ;
+ 
+
+ // 添加RC3 
+define("_MD_AM_ORDER" ,"订单" ) ;
+ 
+define("_MD_AM_ORDER0" ," ( 0 =隐藏)" ) ;
+ 
+define("_MD_AM_ACTIVE" ,"主动" ) ;
+ 
+define("_MD_AM_INACTIVE" ,"无效" ) ;
+ 
+define("_MD_AM_NOTINSTALLED" ,"未安装" ) ;
+ 
+define("_MD_AM_NOCHANGE" ,"没有改变" ) ;
+ 
+define("_MD_AM_INSTALL" ,"安装" ) ;
+ 
+define("_MD_AM_UNINSTALL" ,"卸载" ) ;
+ 
+define("_MD_AM_SUBMIT" ,"提交" ) ;
+ 
+define("_MD_AM_CANCEL" ,"取消" ) ;
+ 
+define("_MD_AM_DBUPDATE" ,"数据库更新成功！" ) ;
+ 
+define("_MD_AM_BTOMADMIN" ,"回到模块管理页面" ) ;
+ 
+
+ // %s的代表模块名称
+define("_MD_AM_FAILINS" ,"无法安装％号" ) ;
+ 
+define("_MD_AM_FAILACT" ,"无法激活％号" ) ;
+ 
+define("_MD_AM_FAILDEACT" ,"无法停用％号" ) ;
+ 
+define("_MD_AM_FAILUPD" ,"无法更新％号" ) ;
+ 
+define("_MD_AM_FAILUNINS" ,"无法卸载％号" ) ;
+ 
+define("_MD_AM_FAILORDER" ,"无法重新％号" ) ;
+ 
+define("_MD_AM_FAILWRITE" ,"无法写入主菜单。" ) ;
+ 
+define("_MD_AM_ALEXISTS" ,"模块的%s已存在。" ) ;
+ 
+define("_MD_AM_ERRORSC" ,"错误(县):");
+ 
+define("_MD_AM_OKINS" ,"模块的%s安装成功。" ) ;
+ 
+define("_MD_AM_OKACT" ,"模块%s的激活成功。" ) ;
+ 
+define("_MD_AM_OKDEACT" ,"模块的%s停用成功。" ) ;
+ 
+define("_MD_AM_OKUPD" ,"模块的%s已成功更新。" ) ;
+ 
+define("_MD_AM_OKUNINS" ," %s的模块卸载成功。" ) ;
+ 
+define("_MD_AM_OKORDER" ,"模块%s的改变成功。" ) ;
+ 
+
+define("_MD_AM_RUSUREINS","按下下面的按钮来安装该模块") ;
+ 
+define("_MD_AM_RUSUREUPD","按下下面的按钮进行更新此模块") ;
+ 
+define("_MD_AM_RUSUREUNINS","您确定要卸载此模块?>") ;
+ 
+define("_MD_AM_LISTUPBLKS","具体包括以下区块将被更新。 <br />选择的区块,其中的内容(范本及期权)可能被改写。 <br />") ;
+ 
+define("_MD_AM_NEWBLKS","新厦") ;
+ 
+define("_MD_AM_DEPREBLKS","不赞成块") ;
+ 
+
+ // 增加2.3 - julionc 
+define("_MD_AM_INSTALLING","安装") ;
+ 
+define("_MD_AM_TABLE_RESERVED","%s是一个保留表！") ;
+ 
+define("_MD_AM_CREATE_TABLES","建立表...");
+ 
+define("_MD_AM_TABLE_CREATED","表%s的创建") ;
+ 
+define("_MD_AM_INSERT_DATA","数据插入到表的%s") ;
+ 
+define("_MD_AM_INSERT_DATA_FAILD","无法插入%s到数据库中。") ;
+ 
+define("_MD_AM_INSERT_DATA_DONE","模块数据插入成功。") ;
+ 
+define("_MD_AM_MODULEID","模块编号： %s的") ;
+ 
+define("_MD_AM_SQL_FOUND","数据库文件发现在%s") ;
+ 
+define("_MD_AM_SQL_NOT_FOUND","数据库找不到文件在%s") ;
+ 
+define("_MD_AM_SQL_NOT_CREATE","错误：无法创建的%s") ;
+ 
+define("_MD_AM_SQL_NOT_VALID","%s不是有效的SQL ！") ;
+ 
+
+define("_MD_AM_GROUP_ID","组ID ： %s的") ;
+ 
+define("_MD_AM_NAME","名称：") ;
+ 
+define("_MD_AM_VALUE","值：") ;
+ 
+
+ /*  模板  */ 
+define("_MD_AM_TEMPLATES_ADD","添加模板...");
+ 
+define("_MD_AM_TEMPLATES_DELETE","删除模板...");
+ 
+define("_MD_AM_TEMPLATES_UPDATE","更新模板...");
+ 
+
+define("_MD_AM_TEMPLATE_ID","模板ID ： %s的") ;
+ 
+
+define("_MD_AM_TEMPLATE_ADD_DATA","%s的模板添加到数据库") ;
+ 
+define("_MD_AM_TEMPLATE_ADD_ERROR","错误：无法插入模板%s到数据库中。") ;
+ 
+define("_MD_AM_TEMPLATE_COMPILED","模板%s的汇编") ;
+ 
+define("_MD_AM_TEMPLATE_COMPILED_FAILED","错误：无法编译模板的%s") ;
+ 
+define("_MD_AM_TEMPLATE_DELETE_DATA","%s的模板中删除该数据库。") ;
+ 
+define("_MD_AM_TEMPLATE_DELETE_DATA_FAILD","错误：无法删除模板%s从数据库。") ;
+ 
+define("_MD_AM_TEMPLATE_INSERT_DATA","%s的模板插入到数据库中。") ;
+ 
+define("_MD_AM_TEMPLATE_RECOMPILE","模板%s的重新") ;
+ 
+define("_MD_AM_TEMPLATE_RECOMPILE_FAILD","错误：模板%s的重新编译失败") ;
+ 
+define("_MD_AM_TEMPLATE_RECOMPILE_ERROR","错误：无法重新编译模板的%s") ;
+ 
+define("_MD_AM_TEMPLATE_DELETE_OLD_ERROR","错误：无法删除旧模板％第堕胎更新此文件。") ;
+ 
+define("_MD_AM_TEMPLATE_UPDATE","模板%s的更新。") ;
+ 
+define("_MD_AM_TEMPLATE_UPDATE_ERROR","错误：无法更新%s的范本。") ;
+ 
+
+ /* 块 */ 
+define("_MD_AM_BLOCKS_ADD","添加区块...");
+ 
+define("_MD_AM_BLOCKS_DELETE","删除块...");
+ 
+define("_MD_AM_BLOCKS_REBUILD","改造区块...");
+ 
+
+define("_MD_AM_BLOCK_ID","查封ID ： %s的") ;
+ 
+
+define("_MD_AM_BLOCK_ACCESS","时间阻止访问权") ;
+ 
+define("_MD_AM_BLOCK_ACCESS_ERROR","错误：无法添加阻止访问权") ;
+ 
+define("_MD_AM_BLOCK_ADD","块的%s说") ;
+ 
+define("_MD_AM_BLOCK_ADD_ERROR","错误：无法新增块%s到数据库！") ;
+ 
+define("_MD_AM_BLOCK_ADD_ERROR_DATABASE","数据库错误： %s的") ;
+ 
+define("_MD_AM_BLOCK_CREATED","块的%s创建") ;
+ 
+define("_MD_AM_BLOCK_DELETE","块的%s删除。") ;
+ 
+define("_MD_AM_BLOCK_DELETE_DATA","%s的块模板从数据库中删除。") ;
+ 
+define("_MD_AM_BLOCK_DELETE_ERROR","错误：无法删除块的%s") ;
+ 
+define("_MD_AM_BLOCK_DELETE_TEMPLATE_ERROR","错误：无法删除块模板%s从数据库") ;
+ 
+define("_MD_AM_BLOCK_DEPRECATED","块模板%s的推荐") ;
+ 
+define("_MD_AM_BLOCK_DEPRECATED_ERROR","错误：无法删除过时块模板。") ;
+ 
+define("_MD_AM_BLOCK_UPDATE","块%s的更新。") ;
+ 
+
+ /*  Configs  */ 
+define("_MD_AM_GONFIG_ID","配置编号： %s的") ;
+ 
+define("_MD_AM_MODULE_DATA_ADD","添加模块配置数据...");
+ 
+define("_MD_AM_MODULE_DATA_DELETE","删除模块配置选项...");
+ 
+define("_MD_AM_MODULE_DATA_UPDATE","模块的数据更新。") ;
+ 
+
+define("_MD_AM_CONFIG_ADD","配置选项说") ;
+ 
+define("_MD_AM_CONFIG_DATA_ADD","%s的配置添加到数据库") ;
+ 
+define("_MD_AM_CONFIG_DATA_ADD_ERROR","错误：无法插入的config %s到数据库中。") ;
+ 
+define("_MD_AM_GONFIG_DATA_DELETE","配置数据从数据库中删除。") ;
+ 
+define("_MD_AM_CONFIG_DATA_DELETE_ERROR","错误：无法删除配置数据从数据库") ;
+ 
+
+ /* 访问 */ 
+define("_MD_AM_GROUP_SETTINGS_ADD","设置组权利...");
+ 
+
+define("_MD_AM_GROUP_PERMS_DELETE_ERROR","错误：无法删除组的权限") ;
+ 
+define("_MD_AM_GROUP_PERMS_DELETED","组权限删除") ;
+ 
+
+define("_MD_AM_ACCESS_ADMIN_ADD","时间管理员权限的组ID权的%s") ;
+ 
+define("_MD_AM_ACCESS_ADMIN_ADD_ERROR","错误：无法添加管理员权限正确的组ID的%s") ;
+ 
+define("_MD_AM_ACCESS_USER_ADD_ERROR","时间用户访问正确的组ID ： %s的") ;
+ 
+define("_MD_AM_ACCESS_USER_ADD_ERROR_ERROR","错误：无法添加用户访问权限的组ID ： %s的") ;
+ 
+
+ // 执行单元的具体安装脚本如有
+define("_MD_AM_FAILED_EXECUTE","无法执行的%s") ;
+ 
+define("_MD_AM_FAILED_SUCESS","%s的执行成功。") ;
+ 
+
+define("_MD_AM_DELETE_ERROR","错误：无法删除的%s") ;
+ 
+define("_MD_AM_UPDATE_ERROR","错误：无法更新的%s") ;
+ 
+define("_MD_AM_DELETE_MOD_TABLES","删除模块表...");
+ 
+
+define("_MD_AM_COMMENTS_DELETE","删除评论...");
+ 
+define("_MD_AM_COMMENTS_DELETE_ERROR","错误：无法删除评论") ;
+ 
+define("_MD_AM_COMMENTS_DELETED","评论删除") ;
+ 
+
+define("_MD_AM_NOTIFICATIONS_DELETE","删除通知...");
+ 
+define("_MD_AM_NOTIFICATIONS_DELETE_ERROR","错误：无法删除的通知") ;
+ 
+define("_MD_AM_NOTIFICATIONS_DELETED","通知删除") ;
+ 
+
+define("_MD_AM_TABLE_DROPPED","表%s的下降！") ;
+ 
+define("_MD_AM_TABLE_DROPPED_ERROR","错误：无法删除表的%s") ;
+ 
+define("_MD_AM_TABLE_DROPPED_FAILDED","错误：不允许下拉表的%s ！") ;
+ 
+
+?>
+
---
+++ releases/1.30/multisite/language/schinese_utf8/modinfo.php
@@ -0,0 +1,22 @@
+<?php // $号： modinfo.php 2879年2009年2月27日0时53分三十四秒ž wishcraft $ 
+ // _LANGCODE ：英
+ // _CHARSET ： UTF - 8的
+ //译者： XOOPS翻译团队
+
+ //模块信息
+
+ //的名字此模块
+define("_MI_MULTISITE_NAME","多点");
+//的简短说明此模块
+define("_MI_MULTISITE_DESC","管理工作的核心设置的多点。");
+//名称管理菜单项
+define("_MI_MULTISITE_ADMENU1","块");
+define("_MI_MULTISITE_ADMENU2","使用偏好");
+define("_MI_MULTISITE_ADMENU3","模块");
+define("_MI_MULTISITE_ADMENU4","网域");
+define("_MI_MULTISITE_ADMENU5","访问策略");
+define("_MI_MULTISITE_BNAME1","主菜单");
+define("_AM_ALLDOMAINS","所有网域");
+define("_AM_MD_MODULES","模块");
+define("ALL_DOMAINS","所有网域");
+?>
