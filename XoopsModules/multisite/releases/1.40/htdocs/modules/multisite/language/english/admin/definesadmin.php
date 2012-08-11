3b871a0ffc96b250e6cd564583200df37923d269
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 07:19:07 +0000

    Removing space from "define (", which is required for Transifex

--- XoopsModules/multisite/releases/1.40/htdocs/modules/multisite/language/english/admin/definesadmin.php
+++ XoopsModules/multisite/releases/1.40/htdocs/modules/multisite/language/english/admin/definesadmin.php
@@ -5,7 +5,7 @@
 	define( '_AM_SELECT_DOMAIN', 'Select the Domain to edit define');
 	define( '_AM_SELECT_DEFINENAME', 'Name:');
 	define( '_AM_SELECT_DEFINEVALUE', 'Value:');
-	define ('_AM_SELECT_DEFINENEW', 'New Define');
+	define('_AM_SELECT_DEFINENEW', 'New Define');
 	define( '_MD_AM_MODDEFINE', 'Defines on this domain');		
 	define( '_MD_AM_DEFADD', 'Add a Define on this domain');
 	define( '_MD_AM_DEFLIST', 'Goto Defines List');
--- XoopsModules/multisite/releases/1.40/htdocs/modules/multisite/language/german/modinfo.php
+++ XoopsModules/multisite/releases/1.40/htdocs/modules/multisite/language/german/modinfo.php
@@ -7,21 +7,21 @@
  // Info-Modul 
 
  // Der Name dieses Moduls 
- define ( "_MI_MULTISITE_NAME", "Multisite"); 
+ define( "_MI_MULTISITE_NAME", "Multisite"); 
 
  // Eine kurze Beschreibung des Moduls 
- define ( "_MI_MULTISITE_DESC", "für die Verwaltung von zentralen Einstellungen des Multi-."); 
+ define( "_MI_MULTISITE_DESC", "für die Verwaltung von zentralen Einstellungen des Multi-."); 
 
  // Namen von admin Menüpunkte 
- define ( "_MI_MULTISITE_ADMENU1", "Blocks"); 
- define ( "_MI_MULTISITE_ADMENU2", "Einstellungen"); 
- define ( "_MI_MULTISITE_ADMENU3", "Modul"); 
- define ( "_MI_MULTISITE_ADMENU4", "Reich"); 
- define ( "_MI_MULTISITE_ADMENU5", "Zugreifen Politik"); 
+ define( "_MI_MULTISITE_ADMENU1", "Blocks"); 
+ define( "_MI_MULTISITE_ADMENU2", "Einstellungen"); 
+ define( "_MI_MULTISITE_ADMENU3", "Modul"); 
+ define( "_MI_MULTISITE_ADMENU4", "Reich"); 
+ define( "_MI_MULTISITE_ADMENU5", "Zugreifen Politik"); 
 
- define ( "_MI_MULTISITE_BNAME1", "Main Menu"); 
+ define( "_MI_MULTISITE_BNAME1", "Main Menu"); 
 
- define ( "_AM_ALLReich", "Alle Reich"); 
- define ( "_AM_MD_MODULES", "Modul"); 
- define ( "_ALL_Reich", "Alle Reich"); 
+ define( "_AM_ALLReich", "Alle Reich"); 
+ define( "_AM_MD_MODULES", "Modul"); 
+ define( "_ALL_Reich", "Alle Reich"); 
  ?> 
